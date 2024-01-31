5415d972da4fc255661c3d98063f2b3be5963cc5
Thomas Brewer <tom.brewer@mac.com>
2024-01-31 14:22:46 -0500

    fix: adding back Redox.Data

---
+++ lib/redox/data.ex
@@ -0,0 +1,16 @@
+defmodule Redox.Data do
+  @moduledoc """
+  A helper module to send requests to Redox API
+  """
+  alias Redox.Request
+
+  @doc """
+  Make a request to the Data Module API
+  """
+  def request(query, access_token) do
+    (Application.get_env(:ex_redox, :redox_url, "https://api.redoxengine.com/") <> "endpoint")
+    |> Request.new(query, access_token)
+    |> Request.put_data()
+    |> Request.send()
+  end
+end
